f311d11d07f8c00432cbaaabbbf2f7fbdec06123
brewchetta <chett.tiller@gmail.com>
2024-05-17 12:05:38 -0400

    memes!

--- src/components/MemeForm.jsx
+++ src/components/MemeForm.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 
-function MemeForm() {
+function MemeForm({ memes, setMemes }) {
 
     const URL = 'http://localhost:3000/memes'
 
+    const [img_url, setImgURL] = useState('')
+    const [caption, setCaption] = useState('')
+
     function handleSubmit(e) {
         e.preventDefault()
+
+        fetch(URL, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+            body: JSON.stringify( { img_url, caption, likes: 0 } )
+            // body: JSON.stringify( { img_url: img_url, caption: caption, likes: 0 } )
+        })
+        .then(res => res.json())
+        .then( newMeme => setMemes( [...memes, newMeme] ) )
+
+        setImgURL('')
+        setCaption('')
     }
 
     return (
@@ -13,11 +28,20 @@ function MemeForm() {
 
             <label htmlFor="img_url">Image URL:</label>
 
-            <input name="img_url" type="text" />
+            <input name="img_url" 
+            type="text"
+            onChange={e => setImgURL(e.target.value)}
+            value={img_url}
+            placeholder='type your image url here'
+            />
 
             <label htmlFor="caption">Caption:</label>
 
-            <input name="caption" type="text" />
+            <input name="caption" 
+            type="text"
+            onChange={e => setCaption(e.target.value.toLowerCase())}
+            value={caption}
+            />
 
             <input type="submit" value="Add Meme" />
 
